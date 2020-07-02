c4d0e500fc385ab904e46267460d79a8d256a76a
Ivan <ivanclair0710@gmail.com>
2020-07-02 09:44:25 +0800

    [story-1] Initial project (#1)
    * [story-1] add needed dependencies
    
    * [story-1] change DI dependency from dagger to hilt
    
    * [story-1] change theme to Material
    
    * [story-1] create MainApplication.kt extend Application and add Hilt annotation
    
    * [story-1] Initial navigation graph

---
+++ app/src/main/java/personal/ivan/migo_quiz/MainApplication.kt
@@ -0,0 +1,7 @@
+package personal.ivan.migo_quiz
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MainApplication : Application()
---
+++ app/src/main/java/personal/ivan/migo_quiz/view/HomeFragment.kt
@@ -0,0 +1,31 @@
+package personal.ivan.migo_quiz.view
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import personal.ivan.migo_quiz.databinding.FragmentHomeBinding
+
+class HomeFragment : Fragment() {
+
+    // View Binding
+    private lateinit var binding: FragmentHomeBinding
+
+    // region Life Cycle
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentHomeBinding.inflate(
+            inflater,
+            container,
+            false
+        )
+        return binding.root
+    }
+
+    // endregion
+}
