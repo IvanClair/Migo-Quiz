993a4bc581385e908c4ef47cabbb8bef6411e8a7
Ivan Kuo <ivanclair0710@gmail.com>
2020-07-02 08:47:36 +0800

    Initial commit

---
+++ app/src/main/java/personal/ivan/migo_quiz/MainActivity.kt
@@ -0,0 +1,11 @@
+package personal.ivan.migo_quiz
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
